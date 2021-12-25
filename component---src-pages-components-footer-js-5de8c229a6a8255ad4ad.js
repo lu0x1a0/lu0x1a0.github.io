af7d155e0db681190e5c0de5d690386b563a185b
Sean Luo's Yoga <41530060+lu0x1a0@users.noreply.github.com>
2021-12-25 17:12:00 +1100

    Updates

---
+++ component---src-pages-components-footer-js-5de8c229a6a8255ad4ad.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio_v2=self.webpackChunkportfolio_v2||[]).push([[215],{26:function(e,t,o){o.r(t),o.d(t,{default:function(){return r}});var n=o(7294);function r(){return n.createElement("section",{id:"Footer"},"This website is powered by React + Gatsby.")}}}]);
+//# sourceMappingURL=component---src-pages-components-footer-js-5de8c229a6a8255ad4ad.js.map
